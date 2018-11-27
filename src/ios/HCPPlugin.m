72740af672a3c2cdd535a4e282c73762caf59c1c
Ray Zeller <rayzeller@gmail.com>
2018-11-27 15:40:04 -0700

    Some logging

--- src/ios/HCPPlugin.m
+++ src/ios/HCPPlugin.m
@@ -278,6 +278,7 @@ static NSString *const DEFAULT_STARTING_PAGE = @"index.html";
 
     // rewrite starting page www folder path: should load from external storage
     if ([self.viewController isKindOfClass:[CDVViewController class]]) {
+        NSLog(@"resetting that bitch %@", _filesStructure.wwwFolder.absoluteString);
         ((CDVViewController *)self.viewController).wwwFolderName = _filesStructure.wwwFolder.absoluteString;
     } else {
         NSLog(@"HotCodePushError: Can't make starting page to be from external storage. Main controller should be of type CDVViewController.");
