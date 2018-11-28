3d10976b00144a45233e7ec294e660c68566762e
Ray Zeller <rayzeller@gmail.com>
2018-11-27 17:23:05 -0700

    formatting and logging

--- src/ios/WKWebViewFix/CDVWKWebViewEngine+HCPPlugin_ReadAccessURL.m
+++ src/ios/WKWebViewFix/CDVWKWebViewEngine+HCPPlugin_ReadAccessURL.m
@@ -17,40 +17,41 @@
 - (id)loadRequest:(NSURLRequest*)request
 {
     if ([self canLoadRequest:request]) { // can load, differentiate between file urls and other schemes
-		CDVViewController* vc = (CDVViewController*)self.viewController;                
+			CDVViewController* vc = (CDVViewController*)self.viewController;                
+			NSLog(@"we are at canLoadRequest %@", vc.startPage);
 
-		//if its contains localhost, the application use local web server
-        if([vc.startPage containsString: @"localhost"]) {
-			NSURLComponents *newUrl = [NSURLComponents componentsWithString:vc.startPage];
+			//if its contains localhost, the application use local web server
+	    if([vc.startPage containsString: @"localhost"]) {
+				NSURLComponents *newUrl = [NSURLComponents componentsWithString:vc.startPage];
 
-			//Case: Navigation after update install complete
-			if (request.URL.fileURL) {
+				//Case: Navigation after update install complete
+				if (request.URL.fileURL) {
 	    		NSString *newPath = [@"/local-filesystem" stringByAppendingString: "_file_/"];
 	        newUrl.path = [ newPath stringByAppendingString: request.URL.path ];
     			NSLog(@"new pathing that other bitch %@", newUrl.path);
 
 			    return [(WKWebView*)self.engineWebView loadRequest:[NSURLRequest requestWithURL:newUrl.URL]];
-			} 
-			//Case: load application start page
-			else {
-				NSURL *wwwPath = [NSURL URLWithString:vc.wwwFolderName];
+				} 
+				//Case: load application start page
+				else {
+					NSURL *wwwPath = [NSURL URLWithString:vc.wwwFolderName];
 			    if(wwwPath.fileURL) {
-			    		NSString *newPath = [@"/local-filesystem" stringByAppendingString: "_file_/"];
-			        newUrl.path = [ newPath stringByAppendingString:[wwwPath.path stringByAppendingString:@"index.html"] ];
-        			NSLog(@"new pathing that bitch %@", newUrl.path);
+		    		NSString *newPath = [@"/local-filesystem" stringByAppendingString: "_file_/"];
+		        newUrl.path = [ newPath stringByAppendingString:[wwwPath.path stringByAppendingString:@"index.html"] ];
+      			NSLog(@"new pathing that bitch %@", newUrl.path);
 
-			        return [(WKWebView*)self.engineWebView loadRequest:[NSURLRequest requestWithURL:newUrl.URL]];
+		        return [(WKWebView*)self.engineWebView loadRequest:[NSURLRequest requestWithURL:newUrl.URL]];
 			    }
 			    else {
-			        return [(WKWebView*)self.engineWebView loadRequest:request];
+		        return [(WKWebView*)self.engineWebView loadRequest:request];
 			    }
+				}
 			}
-		}
-		//Original way
-		else {
-			NSLog(@"Are we even getting here???? %@", request.URL.fileURL);
+			//Original way
+			else {
+				NSLog(@"Are we even getting here???? %@", request.URL.fileURL);
 
-			if (request.URL.fileURL) {
+				if (request.URL.fileURL) {
 			    SEL wk_sel = NSSelectorFromString(CDV_WKWEBVIEW_FILE_URL_LOAD_SELECTOR);
 			    
 			    // by default we set allowingReadAccessToURL property to the plugin's root folder,
@@ -63,24 +64,25 @@
 			    }
 			    
 			    return ((id (*)(id, SEL, id, id))objc_msgSend)(self.engineWebView, wk_sel, request.URL, readAccessUrl);
-			} else {
+				} else {
 			    return [(WKWebView*)self.engineWebView loadRequest:request];
+				}
 			}
-		}
     } else { // can't load, print out error
-        NSString* errorHtml = [NSString stringWithFormat:
-								@"<!doctype html>"
-								@"<title>Error</title>"
-								@"<div style='font-size:2em'>"
-								@"   <p>The WebView engine '%@' is unable to load the request: %@</p>"
-								@"   <p>Most likely the cause of the error is that the loading of file urls is not supported in iOS %@.</p>"
-								@"</div>",							   
-								NSStringFromClass([self class]),
-								[request.URL description],
-								[[UIDevice currentDevice] systemVersion]
-                               ];
+			NSLog(@"Are we even EVEN EVEN getting here???? %@", request.URL.fileURL);
+      NSString* errorHtml = [NSString stringWithFormat:
+							@"<!doctype html>"
+							@"<title>Error</title>"
+							@"<div style='font-size:2em'>"
+							@"   <p>The WebView engine '%@' is unable to load the request: %@</p>"
+							@"   <p>Most likely the cause of the error is that the loading of file urls is not supported in iOS %@.</p>"
+							@"</div>",							   
+							NSStringFromClass([self class]),
+							[request.URL description],
+							[[UIDevice currentDevice] systemVersion]
+                             ];
 
-        return [self loadHTMLString:errorHtml baseURL:nil];
+      return [self loadHTMLString:errorHtml baseURL:nil];
     }
 }
 
